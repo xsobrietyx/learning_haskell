894c8ffbdd5817390428fa4d34697be1e25fa42e
vserdiuk <vadym.serdiuk@gmail.com>
2019-11-04 18:16:01 +0200

    Init

---
+++ first.hs
@@ -0,0 +1,27 @@
+-- Init
+fact x = product [1..x]
+
+double x = x * x
+
+quadro x = double (double x)
+
+a = c + b
+    where
+        c = 1
+        b = 2
+
+-- Interesting one. Three mistakes corrected: (length xs), n, xs tab
+n = a `div` (length xs)
+        where
+            a = 10
+            xs = [1,2,3,4,5]
+
+-- Exercises
+
+-- Custom last by my own hand
+newLast xs = head (drop ((length xs) - 1) xs)
+
+-- Custom init impl, exercise on page 21
+customInit xs = reverse (drop 1 (reverse xs))
+
+customInit2 xs = reverse (tail (reverse xs))
