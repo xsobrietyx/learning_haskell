b815214bed8d952a99a9adc44f7b4a7cbeae543e
vserdiuk <vadym.serdiuk@gmail.com>
2019-11-07 17:38:41 +0200

    Exercise 3 from exercise section 4.8 added.

--- functions.hs
+++ functions.hs
@@ -24,4 +24,31 @@ createEls :: Integral a => a -> [a]
 createEls x = [x..100]
 
 thirdEl :: [a] -> a
-thirdEl (_:_:x:xs) = x
+thirdEl (_:_:x:_) = x
+
+{--Exercises 4.8, page 45--}
+
+halve :: [a] -> ([a],[a]) -- splits even length list into two halves
+halve xs = splitAt ((length xs) `div` 2) xs -- div a b == a `div` b
+
+third :: [a] -> a
+-- pattern matching
+-- third (_:_:x:xs) = x
+-- head and tail
+-- third xs = head (tail (tail xs))
+-- !! indexing
+third xs = ((!!) xs) 2
+
+{--
+    Create safetail function that behaves exactly the same as tail, but returns an empty list in case of empty list.
+    You should use:
+    - conditions
+    - guarded equations
+    - pattern matching
+--}
+safetail :: [a] -> [a]
+-- safetail xs = if (null xs) then [] else (tail xs)
+-- safetail xs | (null xs) = []
+--             | otherwise = tail xs
+safetail [] = []
+safetail (_:xs) = xs
