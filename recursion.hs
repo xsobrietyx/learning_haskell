068e77fb81891075d6f38a71ed1922a4b77b6f11
vserdiuk <vadym.serdiuk@gmail.com>
2019-11-19 18:23:22 +0200

    Exercises added

--- recursion.hs
+++ recursion.hs
@@ -29,3 +29,43 @@ sumdown x = x + sumdown(x-1)
           ->* 2 * 4
           ->* 8
 --}
+
+-- Exercise #4: Euclid's algorithm for finding greatest common devisor
+euclid :: Int -> Int -> Int
+-- example: euclid 6 27 -> 3
+euclid _ 0 = 1
+euclid 0 _ = 1
+euclid a b | a > b = euclid (a - b) b
+           | a < b = euclid a (b - a)
+           | a == b = a
+           
+-- Exercise #5: c prefix means custom
+clength :: [a] -> Int
+clength [] = 0
+clength (x:xs) = 1 + length xs
+{--
+    [1,2,3] -> 1 + length [2,3,[]]
+            -> 1 + 1 + length [3,[]]
+            -> 1 + 1 + 1 + 0
+--}
+
+cdrop :: Int -> [a] -> [a]
+cdrop 0 xs = xs
+cdrop _ [] = []
+cdrop a (x:xs) = cdrop (a-1) xs
+{--
+    2 [1,2,3] -> cdrop (2-1) [2,3,[]]
+              -> cdrop (1-1) [3,[]]
+              -> [3]
+--}
+
+cinit :: [a] -> [a]
+cinit [] = []
+cinit [x] = []
+cinit (x:xs) = x : cinit xs
+{--
+    [1,2,3] -> 1 : cinit [2,3,[]]
+            -> 1 : 2 : cinit [3,[]]
+            -> 1 : 2 : []
+            -> [1,2]
+--}
